dd2cc96b1f747662d5ca1efa759b433109eb7ad6
Thunnathorne <atom9583@gmail.com>
2020-06-09 11:57:48 -0500

    img

--- homepage.js
+++ homepage.js
@@ -1,6 +1,6 @@
 var home = "";
 
-home = home + "<html><center><h1>Homepage</center></h1>";
+home = home + '<html><center><h1>Homepage</center></h1><br><img src="/Users/thunnathorne/Documents/GitHub/NodeProject/OoAtomoO.jpg">';
 
 home = home + "</html>";
 
